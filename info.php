6d9c5aae21b814440e330400612ed9ab766b96f6
cojocosta <diegocostapereira18@gmail.com>
2025-09-16 12:30:12 +0200

    Changes

---
+++ info.php
@@ -0,0 +1,5 @@
+<?php
+// htdocs es donde tenemos los archivos es igual que localhost
+    // phpinfo(); //poner en el buscador: http://localhost/pruebas/info.php
+    echo "<h1> Hello world </h1>;"
+?>
---
+++ info2.php
@@ -0,0 +1,51 @@
+<?php
+    # variables locales y globales
+    $dato1 = 36; //int
+    $dato2 = 15.76; //float
+    $dato3 = true; //boolean
+    $dato4 = "clase"; //string
+    $dato5 = array("Diego", 25, "alumno", true); //array unidimensional de indice
+    
+    print "El valor del dato 1 es: " . $dato1;  //concatenacion con . o , //
+    echo "<br>";
+    echo "El valor del dato 2 es: $dato2";  //concatenacion con . o , //
+    echo "<br>";
+    print var_dump($dato1); //nos dice que tipo de variable es y su valor
+    echo "<br>";
+    print "El valor del dato 2 es: {$dato2}"; 
+    echo "<br>";
+    
+    
+    //constantes y constantes de clase
+    define("CONSTANTE1", "hola mundo");
+    define("CONSTANTE2", array("diego", "pablo", "hugo"));
+    
+    echo CONSTANTE1;
+    echo "<br>";
+    print "El valor de la constante 2 es : " . CONSTANTE2[2];
+    print "<br>";
+    
+    //Definimos una clase
+    class Marca{
+        const CONSTANTE3 = 5.3; //CONSTANTE DE CLASE
+    }
+    print Marca::CONSTANTE3; //uso del operador doble dos puntos (::)
+    print "<br>";
+    
+    //Creamos una funcion para ver el uso de variables de funcion
+    function mostrar(){
+        //acceso a una variable global
+        global $dato1; //variable de fuera de la funcion
+        $dato6 = "variable de función"; //variable de una funcion
+        return "El contenido de la variable dato1 es: $dato1, y el valor de la varible dato6 es: {$dato6}, " . $GLOBALS['dato2'];
+    }
+    print mostrar();
+    print "<br>";
+
+
+
+    //Arrays
+
+    
+    /*Librerias de funciones 
+    y enlazarlas con el programa principal*/
