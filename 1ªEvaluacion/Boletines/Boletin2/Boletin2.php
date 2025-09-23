6e50ecb5accab4df84ff240f77b0415117106b9b
CojoCosta <diegocostapereira18@gmail.com>
2025-09-23 17:21:49 +0200

    changes

--- 1ªEvaluacion/Boletines/Boletin2/Boletin2.php
+++ 1ªEvaluacion/Boletines/Boletin2/Boletin2.php
@@ -6,8 +6,40 @@ function reemplazar(){
 print "Función 1: ";
 print reemplazar() . "<br>";
 
+function contarLetras($frase, $letra){
+    $contador = 0;
+    $longitud = strlen($frase);
+    for($i = 0; $i < $longitud; $i++){
+        if($frase[$i] == $letra){
+            $contador++;
+        }
+    }
+    return $contador;
+}
+print "Función 2: ";
+print contarLetras("Fui a comprar platanos", 'a') . "<br>";
 
+function validarContraseña($contraseña){
+    $primerCaracter = $contraseña[0];
+    return ((strlen($contraseña) >=8 && strlen($contraseña) <=12) && ($primerCaracter >= 0 && $primerCaracter <=9)) ? "Válida" : "No válida";
+}
+print "Función 3: ";
+print validarContraseña("diego") . "<br>";
+print validarContraseña("diego1234") . "<br>";
+print validarContraseña("1234diego") . "<br>";
 
+function numerosPares(){
+    for ($i=0; $i <= 10; $i+2) { 
+        return $numerosPares[$i] = $i;
+    }
+}
+print "Función 4: ";
+print numerosPares() . "<br>";
 
-
+function mesesComM(){
+    $meses=array('enero','febrero','marzo','abril', 'mayo','junio', 'julio','agosto', 'septiembre', 'octubre', 'noviemnbre', 'diciembre');
+    
+}
+print "Función 5: ";
+print mesesComM() . "<br>";
 ?>
