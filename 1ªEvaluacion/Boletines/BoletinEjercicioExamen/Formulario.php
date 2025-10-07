5b0e705191405be469baf4b0cdbeb81ca3488035
Diego Costa <diegocostapereira18@gmail.com>
2025-10-07 11:18:19 +0200

    changes

---
+++ 1ªEvaluacion/Boletines/BoletinEjercicioExamen/Formulario.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de suscripción</title>
+    
+    <style>
+        * {
+            margin-top: 10px;
+        }
+        div{
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <h2 class="display-1">Formulario de suscripción a revistas</h2>
+    <form class="form" action="recogidaDatos.php" method="post">
+        <div>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre">
+            <br>
+            <label for="apellido1">Primer Apellido:</label>
+            <input type="text" name="apellido1" id="apellido1">
+            <br>
+            <label for="apellido2">Segundo apellido:</label>
+            <input type="text" name="apellido2" id="apellido2">
+            <br>
+            <label for="correo">Correo Electrónico:</label>
+            <input type="email" name="correo" id="correo">
+        </div>
+        <div>
+            <label for="interes">¿Desea recicibir información sobra las publicaciones?</label><br>
+            <input type="radio" name="interes" id="si"><span>Sí, estoy interesado</span><br>
+            <input type="radio" name="interes" id="no"><span>No, gracias</span>
+            <br>
+        </div>
+        <div>
+            <label for="checks">Publicaciones disponibles:</label><br>
+            <input type="checkbox" name="national" id=""><span>National Geographic</span><br>
+            <input type="checkbox" name="electronic" id=""><span>Electronic Letters</span><br>
+            <input type="checkbox" name="conocer" id=""><span>Conocer</span><br>
+            <input type="checkbox" name="science" id=""><span>Science</span><br>
+            <input type="checkbox" name="desarrollo" id=""><span>Desarroll Web</span><br>
+        </div>
+        <div>
+            <label for="opciones">Formas de suscripción: </label>
+            <select name="opciones" id="opciones">
+                <option value="elegir" default>Elegir</option>
+                <option value="semanal">Semanal</option>
+                <option value="mensual">Mensual</option>
+                <option value="anual">Anual</option>
+            </select>
+        </div>
+        <button type="submit">Suscribirme</button>
+    </form>
+</body>
+
+</html>
---
+++ 1ªEvaluacion/Boletines/BoletinEjercicioExamen/recogidaDatos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos formulario</title>
+
+    <?php
+
+    ?>
+</head>
+<body>
+    <h1>Datos del suscriptor:</h1><br>
+    
+    
+    
+    
+    
+    
+    <h1>Datos de la suscripción:</h1><br>
+
+</body>
+</html>
