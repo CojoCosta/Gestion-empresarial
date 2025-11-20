067a7a5c23e1f930eacc429eb16c1f5ec5658117
CojoCosta <dcosta1882000@gmail.com>
2025-11-20 13:58:55 +0100

    changes 20/11

--- 1ªEvaluacion/RepasoPOO/Libro.php
+++ 1ªEvaluacion/RepasoPOO/Libro.php
@@ -9,41 +9,77 @@ class Libro
     private static $refLibro = 1;
     public static $numeroDeLibros = 0;
 
-    public function __construct($autor, $titulo, $paginas, $refLibro, $vecesPrestado){
+    public function __construct($autor, $titulo, $paginas, $refLibro, $vecesPrestado)
+    {
         $this->autor = $autor;
         $this->titulo = $titulo;
         $this->paginas = $paginas;
         $this->refLibro = self::$refLibro;
         $this->vecesPrestado = $vecesPrestado;
         $this->vecesPrestado = FALSE;
+        self::$numeroDeLibros++;
     }
 
-#region METODOS GET
-    public function getAutor(){
+    #region METODOS GET
+    public function getAutor()
+    {
         return $this->autor;
     }
 
-    public function getTitulo(){
+    public function getTitulo()
+    {
         return $this->titulo;
     }
 
-    public function getPaginas(){
+    public function getPaginas()
+    {
         return $this->paginas;
     }
-#endregion
-#region METODOS PRINT
-    public function printAutor(){
-        echo " Autor: ".$this->autor;
+    #endregion
+    #region METODOS SET
+    function setRefLibro($refLibro)
+    {
+        if (strlen($refLibro) > 3) {
+            $this->refLibro = $refLibro;
+        } else {
+            echo '<p> Longitud no válida <p>';
+        }
     }
-    public function printTitulo(){
-        echo " Título: ".$this->titulo;
-    }
-    public function printLibro(){
-        echo "Autor: ".$this->autor;
-        echo "Título: ".$this->titulo;
-        echo "Páginas: ".$this->paginas;
-    }
-#endregion
 
+    public function setPrestado()
+    {
+        $this->prestado++;
+    }
+
+    public function setContieneCD()
+    {
+        $this->contieneCD = TRUE;
+    }
+    #endregion
+    #region METODOS PRINT
+    public function printAutor()
+    {
+        echo " Autor: " . $this->autor;
+    }
+    public function printTitulo()
+    {
+        echo " Título: " . $this->titulo;
+    }
+    public function printLibro()
+    {
+        echo "Autor: " . $this->autor . "<br>";
+        echo "Título: " . $this->titulo . "<br>";
+        if (strlen($this->refLibro) > 0) {
+            echo "Referencia: " . $this->refLibro . "<br>";
+        }
+        echo "Libro prestado: " . $this->vecesPrestado . " veces" . "<br>";
+        if ($this->contieneCD) {
+            echo "Si contiene CD" . "<br>";
+        }
+    }
+    #endregion
+#region MAIN
+
+#endregion
 
 }
