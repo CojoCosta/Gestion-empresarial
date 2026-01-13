4989128f1734d48f4a35358c2c8004b2d440bdd0
CojoCosta <dcosta1882000@gmail.com>
2026-01-13 12:22:39 +0100

    changes 13/01

---
+++ canciones_laravel/database/seeders/CancionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CancionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cancions')->insert([
+            [
+                'titulo' => 'OK',
+                'grupo' => 'Rammstein',
+                'album' => 'Zeit',
+                'anio' => '2021'
+            ],
+            [
+                'titulo' => 'Fear of the dark',
+                'grupo' => 'Iron Maiden',
+                'album' => 'Fear of the dark',
+                'anio' => '1990'
+            ],
+            [
+                'titulo' => 'Taurus',
+                'grupo' => 'MikeOldfield',
+                'album' => 'Crises',
+                'anio' => '1986'
+            ]
+        ]);
+    }
+}
